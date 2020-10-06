b36adb0915db6542dc138c6c941586eb21b082e6
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-10-06 13:10:09 -0400

    chore: initial modernization of fixtures etc

--- botfront/imports/ui/components/stories/search/queries.js
+++ botfront/imports/ui/components/stories/search/queries.js
@@ -1,10 +1,11 @@
 import gql from 'graphql-tag';
 
-export const SEARCH_STORIES = gql`
-query stories($projectId: String! $language: String! $queryString: String!) {
-    stories(projectId: $projectId language: $language queryString: $queryString) {
+export const SEARCH_FRAGMENTS = gql`
+query dialogueFragments($projectId: String! $language: String! $queryString: String!) {
+    dialogueFragments(projectId: $projectId language: $language queryString: $queryString) {
         _id
         storyGroupId
+        type
         title
     }
 }`;
